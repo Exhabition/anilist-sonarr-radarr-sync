e362aaa0422b7d65c4852d6b8f42e2f0591c8a0e
Luke <lukeweerheim@hotmail.com>
2023-05-03 18:37:56 +0200

    feat: ID mapping and series monitoring

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
+import { config } from "dotenv";
+config();
+
 import { SchedulerService } from "./helper/SchedulerService";
 import { Radarr } from "./requests/Radarr";
 import { Sonarr } from "./requests/Sonarr";
 
-const hostname = "hostname";
+const hostname = process.env.HOSTNAME;
 
 const sonarrInstance = new Sonarr({
     hostname,
-    apiKey: "sonarrKey",
+    apiKey: process.env.SONARR_API_KEY
 });
 const radarrInstance = new Radarr({
     hostname,
-    apiKey: "radarrKey"
+    apiKey: process.env.RADARR_API_KEY
 });
 
 const scheduler = new SchedulerService(async () => {
@@ -19,6 +22,12 @@ const scheduler = new SchedulerService(async () => {
 
     const isRadarrAlive = await radarrInstance.healthCheck();
     if (isRadarrAlive) console.log("Radarr is healthy");
+
+    // const rootFolders = await sonarrInstance.getRootFolders();
+    // const languageProfiles = await sonarrInstance.getLanguageProfiles();
+    // const qualityprofiles = await sonarrInstance.getQualityProfiles();
+    // console.log(rootFolders, languageProfiles, qualityprofiles);
+    await sonarrInstance.addMedia("e").catch(console.error);
 }, 1000 * 60);
 
 scheduler.start(true);
--- src/requests/RequestClient.ts
+++ src/requests/RequestClient.ts
@@ -1,5 +1,18 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
 
+interface Mappings {
+    "livechart_id": number,
+    "thetvdb_id": number,
+    "anime-planet_id": string,
+    "anisearch_id": number,
+    "anidb_id": number,
+    "kitsu_id": number,
+    "mal_id": number,
+    "type": "OVA" | "MOVIE",
+    "notify.moe_id": string,
+    "anilist_id": number
+}
+
 export interface RequestClientOptions {
     hostname: string;
     apiKey: string;
@@ -34,8 +47,55 @@ export class RequestClient {
         });
     }
 
+    async getAnimeMappings(): Promise<Mappings[]> {
+        return await axios.get<Mappings[]>("https://raw.githubusercontent.com/Fribb/anime-lists/master/anime-list-full.json")
+            .then(res => res.data);
+    }
+
+    async getTVDBIdFromAniListId(aniListId: number) {
+        const mappings = await this.getAnimeMappings();
+        if (!mappings) return console.error("Failed to load mappings");
+
+        const match = mappings.find(info => info.anilist_id === aniListId);
+        return match?.thetvdb_id;
+    }
+
     async healthCheck() {
         const response = await this.client.get<AxiosResponse>("/health").catch(console.error);
         return response && response.status === 200;
+    }
+
+    async getQualityProfiles() {
+        return await this.client.get("/qualityprofile").then(res => res.data);
+    }
+
+    async getLanguageProfiles() {
+        return await this.client.get("/languageprofile").then(res => res.data);
+    }
+
+    async getRootFolders() {
+        return await this.client.get("/rootfolder").then(res => res.data);
+    }
+
+    async addMedia(path: string) {
+        const tvdbId = await this.getTVDBIdFromAniListId(113813);
+
+        const response = await this.client.post<AxiosResponse>("/series", {
+            title: "Rent-a-Girlfriend",
+            tvdbId: tvdbId,
+            languageProfileId: 5,
+            qualityProfileId: 1,
+            rootFolderPath: "/Media/Anime/Series",
+            monitored: true,
+            addOptions: {
+                monitor: "all",
+            }
+        }).catch(error => {
+            console.log(error.response.data);
+        });
+
+        if (!response) throw new Error("Test");
+
+        return response.data;
     }
 }
